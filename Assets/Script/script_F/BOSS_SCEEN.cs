6bba2536df316e826097f427060578b930a156a6
MothilalSurya <akashgametech2024@gmail.com>
2026-04-10 06:19:17 +0530

    1

---
+++ Assets/Script/script_F/BOSS_SCEEN.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BOSS_SCEEN : MonoBehaviour
+{
+  
+
+    public string sceneToLoad; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            SceneManager.GetActiveScene().buildIndex;
+        }
+    }
+}
+}
